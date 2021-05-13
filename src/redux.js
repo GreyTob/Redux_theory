4c2a8a9a5b2bf91b2e322fbefce179df5ffdcf69
Sergey <GreyTob@inbox.ru>
2021-05-13 19:13:50 +0500

    subscribe()

--- src/redux.js
+++ src/redux.js
@@ -7,11 +7,46 @@ const initialState = {
 
 //reducer
 const reduser = (state = initialState, action) => {
+  if (action.type === 'ADD') {
+    return {
+      counter: state.counter + 1,
+    }
+  }
+
+  if (action.type === 'SUB') {
+    return {
+      counter: state.counter - 1,
+    }
+  }
+
+  if (action.type === 'ADD_NUMBER') {
+    return {
+      counter: state.counter - action.value,
+    }
+  }
+
   return state //reducer всегда должен возвращать новый state
 }
 
 //store тут храняться все данные приложенеия. В createStort(передается reducer без вызова)
 const store = redux.createStore(reduser)
-console.log(store.getState())
+//console.log(store.getState())//0 - начальное состояние state
 
-//actions
+//подписаться на изменения state
+store.subscribe(() => {
+  console.log('Subscribe: ', store.getState())
+})
+
+//actions изметяет состояние стора
+const addCounter = {
+  type: 'ADD', //поле type обязательно
+}
+
+store.deispatch(addCounter)
+//console.log(store.getState())//1 - добавили 1
+
+store.deispatch({ type: 'SUB' })
+//console.log(store.getState())//0 - вычли 1
+
+store.deispatch({ type: 'ADD_NUMBER', value: 10 })
+//console.log(store.getState())//10 - добавили value
