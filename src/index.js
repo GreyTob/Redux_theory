dc92905a2d06d3a6d44e2ca8449950e496676f25
Sergey <GreyTob@inbox.ru>
2021-05-17 19:50:29 +0500

    middlewear

--- src/index.js
+++ src/index.js
@@ -3,11 +3,30 @@ import ReactDOM from 'react-dom'
 import './index.module.scss'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import rootReducer from './redux/rootReducer'
 
-const store = createStore(rootReducer)
+// function loggerMiddlewear(store) {
+//   return function (next) {
+//     return function (action) {
+//       //продолжим цепочку выполнения dispatch'ей в store
+//       const result = next(action)
+//       console.log('middlewear', store.getState())
+//       return result
+//     }
+//   }
+// }
+
+//тоже самое но с es6 синтаксисом
+const loggerMiddlewear = (store) => (next) => (action) => {
+  const result = next(action)
+  console.log('middlewear', store.getState())
+  return result
+}
+
+//в applyMiddleware передаются Middleware'ы, которые мы хотим использовать
+const store = createStore(rootReducer, applyMiddleware(loggerMiddlewear))
 
 const app = (
   <Provider store={store}>
